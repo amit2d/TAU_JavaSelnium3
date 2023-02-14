318aa7adf3c399c2356c60a29d6fc43bc5020c08
amit2dworkspace-admin <amit2dworkspace@gmail.com>
2023-02-14 14:30:57 +0530

    Working Example

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -1,29 +1,41 @@
 package testBase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class TestBase {
+
+    static WebDriver driver;
     public void setup () {
         WebDriverManager.chromedriver().setup();
-
-        WebDriver driver = new ChromeDriver();
-        // navigate to site
-        driver.get("https://the-internet.herokuapp.com/");
-
-        //Click on the “Shifting Content” link.
-
-        //Click on the “Shifting Content” link.
-        //Then click on the first example Menu Element.
-        //Then print how many of these menu elements appear.
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.quit();
-
     }
 
     public static void main (String[] args) {
         TestBase test = new TestBase();
         test.setup();
+
+        // navigate to site
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //Click on the “Shifting Content” link.
+        WebElement element = driver.findElement(By.linkText("Shifting Content"));
+        element.click();
+
+        //Then click on the first example Menu Element.
+        element = driver.findElement(By.linkText("Example 1: Menu Element"));
+        element.click();
+
+        //Then print how many of these menu elements appear.
+        List<WebElement> elements = driver.findElements(By.xpath("//ul/li//a"));
+        System.out.println("number of menu elements: " + elements.size());
+
+        driver.quit();
     }
 }
