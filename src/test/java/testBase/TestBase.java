4cf5ee282be60ee9dfa93e18634f301a21e68280
amit <amit2d@yahoo.com>
2023-02-16 23:03:15 +0530

    Frames - examples and Tests

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -1,16 +1,12 @@
 package testBase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
-
-import java.util.List;
 
 public class TestBase {
 
