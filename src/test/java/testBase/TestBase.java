98e5cda193288fe929aa74ae07bc79151fe49ab0
amit <amit2d@yahoo.com>
2023-02-28 20:34:54 +0530

    taking screenshots

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -1,14 +1,22 @@
 package testBase;
 
+import com.google.common.io.Files;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 import util.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class TestBase {
@@ -33,10 +41,19 @@ public class TestBase {
         driver.get("https://the-internet.herokuapp.com/");
     }
 
-
-    @BeforeMethod
-    public void goHome() {
-        driver.get("https://the-internet.herokuapp.com/");
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("D:\\workspace\\TAU_JavaSelenium3\\src\\test\\resources\\screenShots\\test.png"));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
